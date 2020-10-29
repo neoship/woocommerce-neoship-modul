de98b4ff2c940b9eb45f08aa8c7bd15541b94b9e
Miroslav Hutár <hutar@kuskosoft.com>
2020-10-29 08:43:11 +0100

    add free shipping options to carrier

--- includes/class-neoship-wc-parcelshop-shipping-method.php
+++ includes/class-neoship-wc-parcelshop-shipping-method.php
@@ -24,6 +24,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Neoship_WC_Parcelshop_Shipping_Method extends WC_Shipping_Method {
 
 	/**
+	 * Min amount to be free shipping.
+	 *
+	 * @var integer
+	 */
+	public $min_amount = 0;
+
+	/**
+	 * Requires option.
+	 *
+	 * @var string
+	 */
+	public $requires = '';
+
+	/**
 	 * Constructor.
 	 *
 	 * @param int $instance_id Instance id.
@@ -52,9 +66,12 @@ class Neoship_WC_Parcelshop_Shipping_Method extends WC_Shipping_Method {
 		$this->init_settings();
 
 		// Define user set variables.
-		$this->title      = $this->get_option( 'title' );
-		$this->tax_status = $this->get_option( 'tax_status' );
-		$this->cost       = $this->get_option( 'cost' );
+		$this->title      		= $this->get_option( 'title' );
+		$this->tax_status 		= $this->get_option( 'tax_status' );
+		$this->cost       		= $this->get_option( 'cost' );
+		$this->min_amount       = $this->get_option( 'min_amount', 0 );
+		$this->requires         = $this->get_option( 'requires' );
+		$this->ignore_discounts = $this->get_option( 'ignore_discounts' );
 
 		// Actions.
 		add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
@@ -70,7 +87,7 @@ class Neoship_WC_Parcelshop_Shipping_Method extends WC_Shipping_Method {
 			array(
 				'label'   => $this->title,
 				'package' => $package,
-				'cost'    => $this->cost,
+				'cost'    => $this->is_free_shipping_available( $package ) ? 0 : $this->cost,
 			)
 		);
 	}
@@ -99,12 +116,102 @@ class Neoship_WC_Parcelshop_Shipping_Method extends WC_Shipping_Method {
 			),
 			'cost'       => array(
 				'title'       => __( 'Cost', 'woocommerce' ),
-				'type'        => 'text',
+				'type'        => 'price',
 				'placeholder' => '0',
 				'description' => __( 'Optional cost for local pickup.', 'woocommerce' ),
 				'default'     => '',
 				'desc_tip'    => true,
 			),
+			'requires'         => array(
+				'title'   => __( 'Free shipping requires...', 'woocommerce' ),
+				'type'    => 'select',
+				'class'   => 'wc-enhanced-select',
+				'default' => '',
+				'options' => array(
+					''           => __( 'N/A', 'woocommerce' ),
+					'coupon'     => __( 'A valid free shipping coupon', 'woocommerce' ),
+					'min_amount' => __( 'A minimum order amount', 'woocommerce' ),
+					'either'     => __( 'A minimum order amount OR a coupon', 'woocommerce' ),
+					'both'       => __( 'A minimum order amount AND a coupon', 'woocommerce' ),
+				),
+			),
+			'min_amount'       => array(
+				'title'       => __( 'Minimum order amount', 'woocommerce' ),
+				'type'        => 'price',
+				'placeholder' => wc_format_localized_price( 0 ),
+				'description' => __( 'Users will need to spend this amount to get free shipping (if enabled above).', 'woocommerce' ),
+				'default'     => '0',
+				'desc_tip'    => true,
+			),
+			'ignore_discounts' => array(
+				'title'       => __( 'Coupons discounts', 'woocommerce' ),
+				'label'       => __( 'Apply minimum order rule before coupon discount', 'woocommerce' ),
+				'type'        => 'checkbox',
+				'description' => __( 'If checked, free shipping would be available based on pre-discount order amount.', 'woocommerce' ),
+				'default'     => 'no',
+				'desc_tip'    => true,
+			),
 		);
+	}
+	
+	/**
+	 * Check if shipping can be free.
+	 *
+	 * @return boolean
+	 */
+	public function is_free_shipping_available( $package ) {
+		$has_coupon         = false;
+		$has_met_min_amount = false;
+
+		if ( in_array( $this->requires, array( 'coupon', 'either', 'both' ), true ) ) {
+			$coupons = WC()->cart->get_coupons();
+
+			if ( $coupons ) {
+				foreach ( $coupons as $code => $coupon ) {
+					if ( $coupon->is_valid() && $coupon->get_free_shipping() ) {
+						$has_coupon = true;
+						break;
+					}
+				}
+			}
+		}
+
+		if ( in_array( $this->requires, array( 'min_amount', 'either', 'both' ), true ) ) {
+			$total = WC()->cart->get_displayed_subtotal();
+
+			if ( WC()->cart->display_prices_including_tax() ) {
+				$total = $total - WC()->cart->get_discount_tax();
+			}
+
+			if ( 'no' === $this->ignore_discounts ) {
+				$total = $total - WC()->cart->get_discount_total();
+			}
+
+			$total = round( $total, wc_get_price_decimals(), PHP_ROUND_HALF_UP );
+
+			if ( $total >= $this->min_amount ) {
+				$has_met_min_amount = true;
+			}
+		}
+
+		switch ( $this->requires ) {
+			case 'min_amount':
+				$is_available = $has_met_min_amount;
+				break;
+			case 'coupon':
+				$is_available = $has_coupon;
+				break;
+			case 'both':
+				$is_available = $has_met_min_amount && $has_coupon;
+				break;
+			case 'either':
+				$is_available = $has_met_min_amount || $has_coupon;
+				break;
+			default:
+				$is_available = false;
+				break;
+		}
+
+		return $is_available;
 	}
 }
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://neoship.sk/
 Tags: neoship, shipping
 Requires at least: 4.9
 Tested up to: 5.5
-Stable tag: 2.1.0
+Stable tag: 2.2.0
 Requires PHP: 7.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -86,6 +86,9 @@ Fill the form on [https://info.neoship.sk/sk/kontakt](https://info.neoship.sk/sk
 
 = 2.1.0 =
 * Add functionality, select sticker position
+
+= 2.2.0 =
+* Add free shipping options to carriers
 
 == Upgrade Notice ==
 
